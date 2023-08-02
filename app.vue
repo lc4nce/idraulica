a499060a5b564244412e4b611d40addaefd0cc6a
Francesco Guarnieri <Fra.Guarnieri@multiversity.local>
2023-08-02 16:52:52 +0200

    first commit

---
+++ app.vue
@@ -0,0 +1,45 @@
+<script setup>
+const links = ["/"];
+</script>
+<template>
+    <div>
+        <v-app>
+            <v-toolbar color="blue" density="comfortable">
+                <v-toolbar-title>Title</v-toolbar-title>
+                <NuxtLink v-for="link in links" :key="link" :to="link"
+                        ><v-btn
+                            color="white"
+                            variant="text"
+                            class="mx-2"
+                            rounded="xl"
+                            >{{ link }}</v-btn
+                        ></NuxtLink
+                    >
+                <v-spacer></v-spacer>
+                <v-btn icon>
+                    <v-icon>mdi-magnify</v-icon>
+                </v-btn>
+                
+            </v-toolbar>
+            <v-main><NuxtPage /></v-main>
+
+            <v-footer class="bg-grey-lighten-1">
+                <v-row justify="center" no-gutters>
+                    <NuxtLink v-for="link in links" :key="link" :to="link"
+                        ><v-btn
+                            color="white"
+                            variant="text"
+                            class="mx-2"
+                            rounded="xl"
+                            >{{ link }}</v-btn
+                        ></NuxtLink
+                    >
+                    <v-col class="text-center mt-4" cols="12">
+                        {{ new Date().getFullYear() }} —
+                        <strong>Idraulica</strong>
+                    </v-col>
+                </v-row>
+            </v-footer>
+        </v-app>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<script setup>
+const route = useRoute()
+
+const { data } = await useFetch(
+    "http://127.0.0.1:3001/api/review/"
+);
+</script>
+<template>
+    <div >
+        <div v-for="obj in data.response">
+            <v-card
+                class="m-2"
+                :title="obj.product_name"
+                :text="obj.body"
+                variant="tonal"
+            >
+                <v-card-actions>
+                    <NuxtLink :to="obj.product_type + '/' + obj.product_name"
+                        ><v-btn>Leggi la recensione</v-btn></NuxtLink
+                    >
+                </v-card-actions>
+            </v-card>
+        </div>
+    </div>
+</template>
